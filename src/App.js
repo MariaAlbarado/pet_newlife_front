3e488e3f8a90c6724ace5b0907e242cd6a41df88
MariaAlbarado <mariaalbaradoandrade@outlook.com>
2026-04-27 17:04:00 -0300

    implementa login com integração à API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,76 @@
 import { FaPaw } from "react-icons/fa";
+import { useState } from "react";
 
+import "./App.css";
 
-import './App.css';
+function App() {
+  const [cpf, setCpf] = useState("");
+  const [senha, setSenha] = useState("");
 
-function App(){
-  return(
-    <div className='container_inicial'> 
+  async function fazerLogin(evento) {
+    evento.preventDefault();
+    try {
+      const resposta = await fetch("http://localhost:3000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cpf, senha }),
+      });
 
-      
-      <div className='container_titulo'>
-        <h1>< FaPaw/> {""} Pet new Life</h1>
+      const dados = await resposta.json();
+
+      if (resposta.ok) {
+        window.location.href = "/pets";
+      } else {
+        alert(dados.mensage || "Login inválido");
+      }
+    } catch (error) {
+      alert(" Erro ao conectar com a API");
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="container_inicial">
+      <div className="container_titulo">
+        <h1>
+          <FaPaw /> {""} Pet new Life
+        </h1>
         <p>Painel administrativo</p>
       </div>
 
-      
-        <div className='container_imput'>
+      <form onSubmit={fazerLogin}>
+        <div className="container_imput">
           <label>CPF</label>
-          <input type="text" placeholder='000.000.000-00' />
+          <input
+            type="text"
+            placeholder="000.000.000-00"
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
         </div>
 
-        <div className='container_imput'>
+        <div className="container_imput">
           <label>Senha</label>
-          <input type="password" placeholder='********' />
+          <input
+            type="password"
+            placeholder="********"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
         </div>
 
-        <button className='botao_logar' type='submit'>Entrar</button>
-        
-        <p className='paragrafo_login'>Faça login com suas credenciais de funcionário</p>
-      
+        <button className="botao_logar" type="submit">
+          Entrar
+        </button>
+      </form>
+
+      <p className="paragrafo_login">
+        Faça login com suas credenciais de funcionário
+      </p>
     </div>
-
-  )
+  );
 }
-
 
 export default App;
