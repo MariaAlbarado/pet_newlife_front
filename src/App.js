0a017b8dd36137c967ff459cb3440724614012d4
MariaAlbarado <mariaalbaradoandrade@outlook.com>
2026-05-05 14:53:23 -0300

    Adiciona tela de login no App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Menu from "./componentes/Menu";
 import CadastroPet from "./pages/CadastroPet";
 import "./App.css";
+import Login from "./pages/login";
 
 function App() {
   return (
     <>
       <Menu />
-      <CadastroPet />
+      <Login />
     </>
   );
 }
